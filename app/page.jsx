8e40d99e40eaabe6e957041a46ff7abd1434de08
Sahil Kalkal <sahil@flyde.in>
2025-11-25 12:22:08 +0530

    auto generated

---
+++ app/page.jsx
@@ -0,0 +1,6 @@
+import pageData from "../data/pages/home.json";
+import RenderBlock from "../components/RenderBlock";
+
+export default function Page() {
+  return <main>{pageData.components?.map((b,i)=>(<RenderBlock key={i} block={b}/>))}</main>;
+}
---
+++ components/componentRegistry.jsx
@@ -0,0 +1,14 @@
+
+import Header from "./header";
+import HeroSection from "./heroSection";
+import GridVideo from "./GridVideo";
+import WhyChoose from "./whyChoose";
+import FeaturesSection from "./featuresSection";
+
+export const componentRegistry = {
+  "header-sec": Header,
+  "hero-section": HeroSection,
+  "grid-video": GridVideo,
+  "why-choose": WhyChoose,
+  "feature-section": FeaturesSection
+};
